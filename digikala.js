a035794afed2c87aad80ed6e4afbe0c18a72729f
root <root@srv1682246071.bitcommand.net>
2024-04-18 01:18:42 +0000

    autoscroll updated

--- digikala.js
+++ digikala.js
@@ -1,19 +1,24 @@
 // https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+// https://nodejs.org/dist/v20.12.2/node-v20.12.2-linux-x64.tar.xz
+// https://www.digitalocean.com/community/tutorials/how-to-install-node-js-on-ubuntu-22-04
+// https://stackoverflow.com/questions/63312642/how-to-install-node-tar-xz-file-in-linux
 import puppeteer from 'puppeteer-core';
 import fs from 'node:fs';
 import dotenv from 'dotenv'
+import {scrollPageToBottom} from 'puppeteer-autoscroll-down';
+//import scrollPageToBottom from 'puppeteer-autoscroll-down';
 import { v4 as uuidv4} from 'uuid';
 	dotenv.config();
 const myBooleanVar = process.env.HEADLESS;
 // Convert the string value to a boolean
 const headless = myBooleanVar && myBooleanVar === 'true';
 async function digikala (productID, searchQuery) {
-	console.log("digikala is called");
+
   let url = "https://www.digikala.com";
   // Launch the browser and open a new blank page
-  const browser = await puppeteer.launch({headless: headless, executablePath: "chrome-linux/chrome"});
+  const browser = await puppeteer.launch({headless: headless, executablePath: "chrome-linux/chrome", args: ["--no-sandbox"]});
   const page = await browser.newPage();
-await page.setViewport({ width: 1280, height: 800 }); //added for test
+//await page.setViewport({ width: 1280, height: 800 }); //added for test
   await page.setDefaultNavigationTimeout(20000);
   // Navigate the page to a URL
   await page.goto(url);
@@ -37,7 +42,7 @@ await page.setViewport({ width: 1280, height: 800 }); //added for test
     await searchInput.type(searchQuery);
     await page.keyboard.press("Enter");
   }
-  await page.waitForNetworkIdle({waitUntil: 'networkidle0'});
+//  await page.waitForNetworkIdle({waitUntil: 'networkidle0'});
 console.log("سرچ تموم شده و دنبال محصول هستم");
   //here I want to express something more elaborate and difficult.
   //I want to get the node of an element which contains my desired href
@@ -65,22 +70,27 @@ console.log( (product));
   target.type() === 'page' &&  target.opener() === pageTarget);
 const newPage = await newPagePromise.page();
 console.log(newPage.url());
-await newPage.waitForNetworkIdle({waitUntil: 'newtworkidle0'});
+//await newPage.waitForNetworkIdle({waitUntil: 'newtworkidle0'});
 await newPage.waitForNetworkIdle({waitUntil: 'load'});
 await newPage.waitForNetworkIdle({waitUntil: 'newtworkidle2'});
+console.log("new page loaded successfuly");
 //https://byby.dev/js-wait-n-seconds
+if(true){
 //if(Math.random() > 0.5){
-if(Math.random() > 0.5){
-    await autoScroll(newPage);
-}
-else if(Math.random() > 0.5)
+await scrollPageToBottom(newPage, {
+  size: 500,
+  delay: 250
+});
+
+}else if(false)
+//}else if(Math.random() > 0.5)
 {
     console.log("add to cart");
     await newPage.waitForSelector('button[data-testid="add-to-cart"]');
     await newPage.click('button[data-testid="add-to-cart"]');
     await newPage.waitForSelector('a[href*="/checkout/cart"]');
 await newPage.waitForSelector('div[id="base_layout_mobile_footer"]');
-await newPage.waitForNetworkIdle({waitUntil: 'newtworkidle2'});
+//await newPage.waitForNetworkIdle({waitUntil: 'newtworkidle2'});
 console.log("successfully added to cart");
 	}else{
   await newPage.waitForSelector('picture');
@@ -91,6 +101,7 @@ console.log("successfully added to cart");
 
   }
 await newPage.screenshot({path: "logs/" + uuidv4() + ".png"});
+console.log("picture saved");
 await page.close();
 await newPage.close();
   await browser.close();
@@ -152,8 +163,13 @@ const lines = file.split(/\r?\n/);
             const splitAt = line.indexOf(' ');
             const link = line.substring(0, splitAt);
             const query = line.substring(splitAt + 1, line.length);
+try{
             await digikala(link, query);
-        }else {
+}catch(e){
+
+console.log(e);
+}    
+    }else {
 	break;
 	}
     }
